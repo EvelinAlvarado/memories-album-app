dd08dbd373891d11cc2d5b3401d17f98ca85e1a4
EvelinAlvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-08-30 15:00:00 +0100

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,9 @@ import axios from "axios";
 
 // Create a instance because is recommendable.
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  // If you're running this project locally, uncomment the following line:
+  //baseURL: "http://localhost:3000",
+
+  // If you're using the online version, leave this as is:
+  baseURL: "https://json-server-memories-album-app.vercel.app/",
 });
